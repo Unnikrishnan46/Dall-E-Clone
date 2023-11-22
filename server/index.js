5e128c8fa3500770913fb616030d9b5304bb9368
Unnikrishnan46 <erayamcode2004@gmail.com>
2023-11-22 17:01:16 +0530

    no cors 1

--- server/index.js
+++ server/index.js
@@ -11,16 +11,6 @@ const app = express();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://dall-e-client-two.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
