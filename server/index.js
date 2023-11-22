989e49c455dfadedd69c99f408b0e922fc2ddc27
Unnikrishnan46 <erayamcode2004@gmail.com>
2023-11-22 16:50:16 +0530

    setting cors settings

--- server/index.js
+++ server/index.js
@@ -1,33 +1,42 @@
 import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import * as dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
-
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post',postRoutes);
-app.use('/api/v1/dalle',dalleRoutes)
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://dall-e-client-two.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
-app.get('/', async (req, res) => {
-    res.send("Hello from DALL-E");
-})
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E");
+});
 
 const startServer = async () => {
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log("Server has started on port 8080"))
-    } catch (error) {
-        console.log(error)
-    }
-    
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log("Server has started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-startServer()
+startServer();
